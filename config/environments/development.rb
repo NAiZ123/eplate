e912f1a1ff85024bc56782261da9a8721f4b70be
NAiZ <entotu_4@outlook.jp>
2020-08-22 06:46:32 +0000

    hosting設定を行末へ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  
-  config.hosts = nil
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -61,4 +59,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+
+  config.hosts = nil
 end
--- config/initializers/blocked_host.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.configure do
-  config.hosts << "localhost"
-  config.hosts << "e-plate.herokuapp.com"
-end
