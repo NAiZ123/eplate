24deb4a5022130a780fcd1f6df5fb58e69b8861d
NAiZ <entotu_4@outlook.jp>
2020-08-23 04:15:25 +0000

    add login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   helper_method :current_user
-
+  before_action :login_required
 
   private 
 
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-
+  def login_required
+    redirect_to login_path unless current_user
+  end
 
 end
