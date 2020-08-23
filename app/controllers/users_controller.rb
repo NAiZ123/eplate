94d47e1ee8fee5c787a327248547660aebee1500
NAiZ <entotu_4@outlook.jp>
2020-08-23 11:55:47 +0000

    add signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-  
+
+  skip_before_action :login_required
+
   def show
     @user = User.find(params[:id])
   end
   
   def new
+    @user = User.new
   end
 end
