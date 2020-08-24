442f487811a0e251315030878b1490695928c7dc
NAiZ <entotu_4@outlook.jp>
2020-08-24 09:11:36 +0000

    add signup form

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 6.0.3'
 gem 'bootstrap-sass', '~> 3.4.1' 
 gem 'sassc-rails', '>= 2.1.0'
 gem 'jquery-rails' , '4.3.1'
+gem 'rails-i18n'
 
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  skip_before_action :login_required
+  skip_before_action :login_required, only: [:new, :create, :show]
 
   def show
     @user = User.find(params[:id])
@@ -9,4 +9,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
+
 end
